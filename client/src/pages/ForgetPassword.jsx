a0853faa09eaac6a4c800a324f57f42d82f9b36d
MMundanu <mmundanu@yahoo.com>
2023-02-09 19:45:47 -0300

    forgetPassword

--- client/src/pages/ForgetPassword.jsx
+++ client/src/pages/ForgetPassword.jsx
@@ -1,11 +1,67 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Swal from "sweetalert2";
+import { Alert } from "../components/Alert";
+import { clientAxios } from "../config/clientAxios";
+
 
 export const ForgetPassword = () => {
+
+    const [alert, setAlert] = useState({});
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if(!email){
+            handleShowAlert('El email es requerido')
+            return null
+        }
+
+        try {
+
+            const {data} = await clientAxios.post('/auth/send-token',{
+                email
+            })
+            
+            Swal.fire({
+                icon: 'info',
+                title: 'Revisa tu casilla de email',
+                text: data.msg,
+                confirmButtonText : "Entendido",
+                allowOutsideClick : false
+              });
+              
+              setEmail('')
+
+        } catch (error) {
+            console.log(error)
+            handleShowAlert(error.response?.data.msg.message)
+            setEmail('')
+        }
+    }
+
+    const handleShowAlert = (msg) => {
+        setAlert({
+            msg
+        });
+        setTimeout(() => {
+            setAlert({})
+        }, 3000)
+    }
+
+
   return (
     <div>       
     <h1>Recupera tu acceso</h1>
+    {
+        alert.msg && <Alert {...alert}/>
+    }
 
-    <form action="">
+    <form action="" 
+    onSubmit={handleSubmit}
+    noValidate
+    >
 
     
 
@@ -15,6 +71,8 @@ export const ForgetPassword = () => {
             type="email"
             id="email"
             placeholder="Ingrese su email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             />
         </div>
         
